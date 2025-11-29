db01a6d4d37f5e08e84a95a766fed63a7a78ca41
Mariam091990 <taskiamariam09@gmail.com>
2025-11-29 21:21:23 +0600

    now server data showing clearly

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-//   cors por space ===>cant find cors showing ***
+
 
 require('dotenv').config();
-//  console.log(process.env.DB_USER);
-//  console.log(process.env.DB_PASS);
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
@@ -21,7 +20,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster09.ceody5c.mongodb.net/?appName=Cluster09`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -32,8 +31,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
+   
 
     const plantsCollection = client.db('plantDB').collection('plants')
 
@@ -73,15 +72,7 @@ async function run() {
         $set: updatedPlant
       }
 
-      //       // const updatedDoc ={
-      //      $set :{
-      //           name: updatedPlant.name,
-      //          category: updatedPlant.category,
-      //         ...
-      //         ...
-      // eivabe each name r value likha jay elaborately likhle .kintu ta drkr nai karon updatedPlant j send kora holo fronend theke req.body te kore. inspec e dekle dekha jabe amoni ekta object send korche tai back end eivabe vengge likha drkr nai.
-      //      }  
-      // }
+      
 
       const result = await plantsCollection.updateOne(filter, updatedDoc, options);
       res.send(result);
@@ -100,21 +91,34 @@ async function run() {
     })
 
 
+    app.get("/plants", async (req, res) => {
+      const sortBy = req.query.sortBy;
+
+      let sortQuery = {};
+
+      if (sortBy === "date") {
+        sortQuery = { createdAt: -1 }; // newest first
+      }
+
+      const plants = await plantsCollection.find().sort(sortQuery).toArray();
+
+      res.send(plants);
+    });
+
 
 
 
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   }
 
 
 
   finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
 run().catch(console.dir);
